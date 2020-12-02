2c6277ab55042995563490ad1808d5a09f443405
Cristhian Daza <cristian.d.b@hotmail.com>
2020-12-01 20:46:30 -0500

    Agregando Hounter a la lista

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GifApp = () => {
+  // const categories = ['Dragon Ball', 'Samurai X']
+  const [categories, setCategories] = useState(['Dragon Ball', 'Samurai X'])
+  const handleAdd = () => {
+    setCategories(['Hunter', ...categories])
+  }
+
   return (
     <>
       <h1>Gif Search ⚡</h1>
       <hr/>
+      <input id="texto" type="text"/>
+      <button onClick={handleAdd}>Agregar</button>
+      <ol>
+        {
+          categories.map(categorie => {
+            return <li key={categorie}>{categorie}</li>
+          })
+        }
+      </ol>
     </>
   );
 }
