9577bdea78e28289500c9a9291020259bf7f5e45
Cristhian Daza <cristian.d.b@hotmail.com>
2020-12-02 21:57:47 -0500

    Agregando helper para el llamado a la api

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,30 +1,14 @@
 import React, { useEffect, useState } from 'react'
+import { getGif } from '../helpers/getGifs'
 import { GifGridItem } from './GifGridItem'
 
 export const GifGrid = ({category}) => {
-  const api = process.env.REACT_APP_API
-
   const [images, setImages] = useState([])
 
   useEffect(() => {
-    getGif()
-  }, [])
-
-  const getGif = async () => {
-
-    const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=${api}`
-    const res = await fetch(url)
-    const { data } = await res.json()
-    const gifs = data.map( img => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_large.url
-      }
-    })
-    console.log(data)
-    setImages(gifs)
-  }
+    getGif(category)
+      .then(setImages)
+  }, [category])
 
   return (
     <>
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+const api = process.env.REACT_APP_API
+
+export const getGif = async (category) => {
+
+  const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=${api}`
+  const res = await fetch(url)
+  const { data } = await res.json()
+  const gifs = data.map( img => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_large.url
+    }
+  })
+
+  return gifs
+}
