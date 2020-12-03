737384dfd932da7ad26c3ad9b74b4e1b0d9c76a7
Cristhian Daza <cristian.d.b@hotmail.com>
2020-12-02 22:33:20 -0500

    Agregando animacion al mostrar las cards y mostrando 12 cards

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,14 +2,14 @@ const api = process.env.REACT_APP_API
 
 export const getGif = async (category) => {
 
-  const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=${api}`
+  const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=12&api_key=${api}`
   const res = await fetch(url)
   const { data } = await res.json()
   const gifs = data.map( img => {
     return {
       id: img.id,
       title: img.title,
-      url: img.images?.downsized_large.url
+      url: img.images?.downsized_medium.url
     }
   })
 
